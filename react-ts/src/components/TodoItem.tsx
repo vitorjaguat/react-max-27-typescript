adc7c7b3f1fac3ab452543fb66361d71f55bb5c8
Vitor Butkus <vitorbutkus@gmail.com>
2023-04-03 12:04:21 -0300

    add content

--- react-ts/src/components/TodoItem.tsx
+++ react-ts/src/components/TodoItem.tsx
@@ -3,7 +3,7 @@ import classes from './TodoItem.module.css';
 
 const TodoItem: React.FC<{
   text: string;
-  onRemoveTodo: () => void;
+  onRemoveTodo: (id) => void;
 }> = (props) => {
   return (
     <li className={classes.item} onClick={props.onRemoveTodo}>
